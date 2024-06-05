7c3a12162d01ee361dc056b16d39a626ffaa63c8
JihShian <50051633+JihShian@users.noreply.github.com>
2024-06-05 09:59:19 +0000

    update training model

--- train_model.py
+++ train_model.py
@@ -54,7 +54,8 @@ def preprocess_and_train_model(data):
     best_model = XGBRegressor(**best_params)
     best_model.fit(X_train, y_train)
 
-    return model
+    return best_model
+
 
 # Load data
 data = pd.read_csv('cleaned_data.csv').copy()
